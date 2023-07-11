3b154bf761e05b3059b1ec6d699c2f4a15a972f6
oslob99 <jkh991116@naver.com>
2023-07-11 16:23:10 +0900

    Initial Commit

---
+++ src/main/java/com/oslob99/oslob99/user/api/UserController.java
@@ -0,0 +1,11 @@
+package com.oslob99.oslob99.user.api;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/login")
+public class UserController {
+}
