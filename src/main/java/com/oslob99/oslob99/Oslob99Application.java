abffe4aa26b6ae8bbe47d10b428076083dfe7c68
oslob99 <jkh991116@naver.com>
2023-07-11 16:20:26 +0900

    Initial Commit

---
+++ src/main/java/com/oslob99/oslob99/Oslob99Application.java
@@ -0,0 +1,13 @@
+package com.oslob99.oslob99;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oslob99Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oslob99Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/oslob99/oslob99/Oslob99ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oslob99.oslob99;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oslob99ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
